efe9f7a7fce2e69a4a1826dbd5c9ab19b635dff7
MaryJane <you@example.com>
2026-09-11 02:24:54 +0100

    chore: updated the config package

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
 	"github.com/MaryJane-09/nexus/backend/config"
 	"github.com/MaryJane-09/nexus/backend/internal/router"
 )
 
 func main() {
-	fmt.Println("Nexus server running on port", config.ServerPort)
+
+	fmt.Println("Nexus server running on port", config.AppConfig.ServerPort)
 	server := router.New()
-	err := http.ListenAndServe(config.ServerPort, server)
+	err := http.ListenAndServe(config.AppConfig.ServerPort, server)
 	if err != nil {
 		fmt.Println("Failed to start HTTP server: ", err)
 	}
--- backend/config/config.go
+++ backend/config/config.go
@@ -1,3 +1,18 @@
 package config
 
-var ServerPort = ":8080"
+import (
+	"os"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+type Config struct {
+	ServerPort       string
+	GmailAddress     string
+	GmailAppPassword string
+}
+
+var AppConfig = Config{
+	ServerPort:       ":8080",
+	GmailAddress:     os.Getenv("GMAIL_ADDRESS"),
+	GmailAppPassword: os.Getenv("GMAIL_APP_PASSWORD"),
+}
