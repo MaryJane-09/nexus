e142568ac008bf9a1bddd7c483c55f457dd2e37f
MaryJane-09 <nzekwechinaza0613@gmail.com>
2026-09-17 12:12:20 +0100

    update: built my connect function

---
+++ backend/internal/db/connection.go
@@ -0,0 +1,17 @@
+package db
+
+import (
+	"context"
+	"github.com/MaryJane-09/nexus/backend/config"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func Connect() (*pgxpool.Pool, error){
+	connString := config.AppConfig.DatabaseURL
+	ctx := context.Background()
+	pool, err := pgxpool.New(ctx, connString)
+	if err != nil{
+		return nil, err
+	}
+	return pool, nil
+}
