151c270590650e320e14301846487f2a1b528977
MaryJane-09 <nzekwechinaza0613@gmail.com>
2026-09-17 11:42:46 +0100

    feat: connecting my postgres database to my backend

--- backend/config/config.go
+++ backend/config/config.go
@@ -10,11 +10,12 @@ type Config struct {
 	ServerPort       string
 	GmailAddress     string
 	GmailAppPassword string
+	DatabaseURL string
 }
 
 var AppConfig = Config{
 	ServerPort:       ":8080",
 	GmailAddress:     os.Getenv("GMAIL_ADDRESS"),
 	GmailAppPassword: os.Getenv("GMAIL_APP_PASSWORD"),
-	//DatabaseURL:      os.Getenv("DATABASE_URL"),
+	DatabaseURL:      os.Getenv("DATABASE_URL"),
 }
